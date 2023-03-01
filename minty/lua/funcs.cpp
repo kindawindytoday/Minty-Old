b4782bb51c43acb2d5166fab9e6328c0cc675e60
mintygingy <bnctfbdb@gmail.com>
2023-03-02 02:29:38 +0300

    working at lua hook

---
+++ minty/lua/funcs.cpp
@@ -0,0 +1,13 @@
+extern "C" {
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+}
+
+void func_changeuid()
+{
+	const char* func_changeuid_script = "CS.MoleMole.ActorUtils.ShowMessage(\"ok imagine it is uid code\")";
+	lua_State* L = luaL_newstate();
+	luaL_dostring(L, func_changeuid_script);
+	lua_close(L);
+}
