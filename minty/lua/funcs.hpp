829e385584ffdfd83ba54b88d2898211b6028582
Moistcrafter <63527712+Moistcrafter@users.noreply.github.com>
2023-03-06 20:11:58 -0800

    Browser toggle & various changes..

--- minty/lua/funcs.hpp
+++ minty/lua/funcs.hpp
@@ -28,8 +28,62 @@ local success, result = pcall(function() \
         end \
         xpcall(normalattack, onError)";
 
-const char* char_browser_on = "";
-const char* char_browser_off = "";
+const char* char_browser_on = R"MY_DELIMITER(
+
+local function findActiveAvatar()
+    local avatarRoot = CS.UnityEngine.GameObject.Find("/EntityRoot/AvatarRoot")
+    if avatarRoot.transform.childCount == 0 then
+        return
+    end
+    for i = 0, avatarRoot.transform.childCount - 1 do
+        local avatar = avatarRoot.transform:GetChild(i)
+        if avatar.gameObject.activeInHierarchy then
+            return avatar.gameObject
+        end
+    end
+end
+
+local function findAvatarBody(avatar)
+    for i = 0, avatar.transform.childCount - 1 do
+        local transform = avatar.transform:GetChild(i)
+        if transform.name == "OffsetDummy" then
+            for j = 0, transform.childCount - 1 do
+                local child = transform:GetChild(j)
+                for k = 0, child.transform.childCount - 1 do
+                    local body = child.transform:GetChild(k)
+                    if body.name == "Body" then
+                        return body.gameObject
+                    end
+                end
+            end
+        end
+    end
+end
+local function createPrimitive()
+    local nowAvatar = findActiveAvatar()
+    local nowBody = findAvatarBody(nowAvatar)
+    local primitive = CS.UnityEngine.GameObject.CreatePrimitive(CS.UnityEngine.PrimitiveType.Plane);
+    primitive.transform.localScale = CS.UnityEngine.Vector3(3.2,1,1.8);
+    primitive.transform.localRotation = CS.UnityEngine.Quaternion(0.5, 0.5,-0.5, 0.5);
+	primitive.name = "BrowserObject"
+    local position = nowBody.transform.position
+    primitive.transform.position = CS.UnityEngine.Vector3(position.x, position.y + 5, position.z);
+    local browser = primitive:AddComponent(typeof(CS.ZenFulcrum.EmbeddedBrowser.Browser));
+	browser._width = 1920;
+    browser._height = 1080;
+    browser._url = "https://google.com";
+
+end
+function start()
+    createPrimitive()
+end
+
+start()
+
+)MY_DELIMITER";
+const char* char_browser_off = R"MY_DELIMITER(
+CS.UnityEngine.Object.Destroy(CS.UnityEngine.GameObject.Find("/BrowserObject"))
+)MY_DELIMITER";
 
 const char* char_bub_initiate = R"MY_DELIMITER(
 local function findHierarchyPath(child)
@@ -137,6 +191,10 @@ local function FindOffsetDummy()
 
 CS.UnityEngine.GameObject.Find(FindOffsetDummy() .. "/Bip001/Bip001 Pelvis/Bip001 Spine/Bip001 Spine1/Bip001 Spine2/boba").transform.localScale = CS.UnityEngine.Vector3()MY_DELIMITER";
 
+const char* char_openarea_fog = R"MY_DELIMITER(CS.UnityEngine.GameObject.Find("/LevelMapUIManager(Clone)/Canvas3D/MapBackContainer/BigWorld_Map(Clone)/MapArea/OpenArea_Fog"):SetActive()MY_DELIMITER";
+
+const char* char_guidearea_fog = R"MY_DELIMITER(CS.UnityEngine.GameObject.Find("/LevelMapUIManager(Clone)/Canvas3D/MapBackContainer/BigWorld_Map(Clone)/GuideArea"):SetActive()MY_DELIMITER";
+
 void luahookfunc(const char* charLuaScript) {
 
 	auto compiled = compile(gi_LL, charLuaScript);
@@ -151,16 +209,3 @@ void luahookfunc(const char* charLuaScript) {
 	};
 	QueueUserAPC(execute, main_thread, (ULONG_PTR)copy);
 };
-
-//void func_changeuid() {
-//
-//	auto compiled = compile(gi_LL, "CS.UnityEngine.GameObject.Find(\"/BetaWatermarkCanvas(Clone)/Panel/TxtUID\"):GetComponent(\"Text\").text = \"<b><i><color = #11edb1>KINDA WINDY TODAY</color></i></b>\"");
-//	auto copy = new std::string(compiled.value());
-//	auto execute = [](ULONG_PTR compiled)
-//	{
-//		auto str = (const std::string*)compiled;
-//		exec(*str);
-//		delete str;
-//	};
-//	QueueUserAPC(execute, main_thread, (ULONG_PTR)copy);
-//};
--- minty/main.cpp
+++ minty/main.cpp
@@ -72,30 +72,17 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 			return oPresent(pSwapChain, SyncInterval, Flags);
 	}
 
-	static bool isopened = true;
-	if (ImGui::IsKeyDown(ImGui::GetKeyIndex(ImGuiKey_F12)))
-	{
-		isopened = !isopened;
-	}
-	
-	//whole imgui menu code in this if(isopened) -- nvm not wokin
-	if (isopened) 
-	{
 		ImGui_ImplDX11_NewFrame();
 		ImGui_ImplWin32_NewFrame();
 
 		// imgui code between newframe and render
 
 		ImGui::NewFrame();
-		ImGui::GetStyle().IndentSpacing = 7.0f;
+		ImGui::GetStyle().IndentSpacing = 16.0f;
+		setlocale(LC_ALL, "C");
 		static float TimeScale = 1.0f;
 		static bool showEditor = false;
-
-		if (ImGui::IsKeyDown(ImGui::GetKeyIndex(ImGuiKey_F12)))
-		{
-			isopened = !isopened;
-		}
-
+		
 		if (ImGui::IsKeyDown(ImGui::GetKeyIndex(ImGuiKey_F11)))
 		{
 			TimeScale = 1.0f;
@@ -106,26 +93,10 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 		ImGui::Begin("Minty");
 		//ImGui::PushFont(font);
 		ImGui::BeginTabBar("Minty");
-		//auto gi_LL = luaL_newstate();
-
-
-		/*if (ImGui::IsKeyDown(ImGui::GetKeyIndex(ImGuiKey_F12)))
-		{
-			isopened = !isopened;
-		}*/
 
 		if (ImGui::BeginTabItem("Lua"))
 		{
-			/*if (ImGui::Button("Change UID")) {
-
-				luahookfunc(char_changeuid);
-			}*/
-
-			/*if (ImGui::Button("Change Elemental Inf (visual)")) {
-				luahookfunc(char_eleminf);
-			}*/
-
-			/*static bool browser_state = false; ------ wip
+			static bool browser_state = false; 
 			if (ImGui::Checkbox("Spawn Browser", &browser_state)) {
 				if (browser_state) {
 					luahookfunc(char_browser_on);
@@ -133,18 +104,16 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 				else {
 					luahookfunc(char_browser_off);
 				}
-			}*/
-
-			static float boob_size = 1.0f;
-			static bool show_resizer = false;
-
-			if (ImGui::Checkbox("booba resizer", &show_resizer))
-			{
 			}
+
+			static bool show_resizer = false;
+			static float boob_size = 1.0f;
+			if (ImGui::Checkbox("booba resizer", &show_resizer)){}
 
 			if (show_resizer)
 			{
-				ImGui::SameLine();
+				ImGui::Indent();
+
 				if (ImGui::Button("Initiate resize")) {
 					boob_size = 1.0f;
 					luahookfunc(char_bub_initiate);
@@ -155,12 +124,31 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 					std::string result = char_bub_resize + std::to_string(boob_size) + "," + std::to_string(boob_size) + "," + std::to_string(boob_size) + ")";
 					luahookfunc(result.c_str());
 				}
+				ImGui::Unindent();
 			}
 
-			if (ImGui::Checkbox("Lua editor", &showEditor))
-			{
-				// Checkbox value has changed, do something here
-			}
+			// static bool map_fog = false;
+			// if (ImGui::Checkbox("Remove map fog",  &map_fog)){}
+			// static bool remove_openarea_fog = false;
+			// static bool remove_guidearea_fog = false;
+			// if (map_fog)
+			// {
+			// 	ImGui::Indent();
+
+			// 	if (ImGui::Checkbox("Remove OpenArea fog", &remove_openarea_fog)) {
+			// 		std::string result = char_openarea_fog + std::to_string(remove_openarea_fog) + ")";
+			// 		luahookfunc(result.c_str());
+			// 	}
+			// 	if (ImGui::Checkbox("Remove GuideArea fog", &remove_guidearea_fog)) {
+			// 		std::string result = char_guidearea_fog + std::to_string(remove_guidearea_fog) + ")";
+			// 		luahookfunc(result.c_str());
+			// 	}
+
+			// 	ImGui::Unindent();
+			// }
+
+			if (ImGui::Checkbox("Lua editor", &showEditor)){}
+
 			static char inputTextBuffer[512] = "";
 
 			ImGui::InputTextWithHint("##input", "Enter custom UID text here...", inputTextBuffer, sizeof(inputTextBuffer));
@@ -178,10 +166,10 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 			}
 			ImGui::SameLine();
 			if (ImGui::Button("Reset")) {
+				TimeScale = 1.0f;
 				std::string result = "CS.UnityEngine.Time.timeScale = 1.0";
 				luahookfunc(result.c_str());
 			}
-
 
 			ImGui::EndTabItem();
 		}
@@ -193,7 +181,6 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 
 			if (!initialized)
 			{
-				// Set the initial code and editor options
 				editor.SetLanguageDefinition(TextEditor::LanguageDefinition::Lua());
 				editor.SetPalette(TextEditor::GetDarkPalette());
 
@@ -206,10 +193,8 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 
 			if (ImGui::Button("Run"))
 			{
-				// Retrieve the text from the TextEditor
 				std::string code = editor.GetText();
 
-				// Call your function with the code string
 				if (!code.empty())
 				{
 					luahookfunc(code.c_str());
@@ -253,12 +238,11 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 			{
 				if (ImGui::BeginMenu("File"))
 				{
-					/*if (ImGui::MenuItem("Save")) ------ wip
+					if (ImGui::MenuItem("Save as button"))
 					{
-						auto textToSave = editor.GetText();
-						/// save text....
+
 					}
-					ImGui::EndMenu();*/
+					ImGui::EndMenu();
 				}
 				if (ImGui::BeginMenu("Edit"))
 				{
@@ -340,7 +324,7 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 			// Content for About
 			ImGui::Text("Minty v0.5");
 			ImGui::Text("ImGui version: %s", ImGui::GetVersion());
-			setlocale(LC_ALL, "Russian");
+			
 			ImGui::Text("Contributors: мятный пряник#0086, Moistcrafter#9172, yarik#4571, azzu#2731");
 			ImGui::Text("");
 			ImGui::Text("KWT Team Discord (click)");
@@ -360,12 +344,12 @@ HRESULT __stdcall hkPresent(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT 
 		ImGui::End();
 		ImGui::EndFrame();
 		ImGui::Render();
-	}
+
 	//end of imgui code
 	pContext->OMSetRenderTargets(1, &mainRenderTargetView, NULL);
 	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 	return oPresent(pSwapChain, SyncInterval, Flags);
-}
+	}
 
 //DWORD WINAPI MainThread(LPVOID lpReserved)
 //{
