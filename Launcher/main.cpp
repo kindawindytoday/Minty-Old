b899e63cdfbf3882e032c8eed6d104389049eb65
rrryfu <83335294+mangorifo@users.noreply.github.com>
2023-04-06 12:53:52 +0800

    Update main.cpp

--- Launcher/main.cpp
+++ Launcher/main.cpp
@@ -9,6 +9,7 @@
 //#include "../minty/gilua/util.h"
 //#include "../minty/gilua/luahook.h"
 
+using namespace std;
 namespace fs = std::filesystem;
 
 namespace util
@@ -37,20 +38,20 @@ bool InjectStandard(HANDLE hTarget, const char* dllpath)
     LPVOID dllPathAddr = VirtualAllocEx(hTarget, NULL, strlen(dllpath) + 1, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
     if (dllPathAddr == NULL)
     {
-        std::cout << "Failed allocating memory in the target process. GetLastError(): " << GetLastError() << "\n";
+        cout << "Failed allocating memory in the target process. GetLastError(): " << GetLastError() << "\n";
         return false;
     }
 
     if (!WriteProcessMemory(hTarget, dllPathAddr, dllpath, strlen(dllpath) + 1, NULL))
     {
-        std::cout << "Failed writing to process. GetLastError(): " << GetLastError() << "\n";
+        cout << "Failed writing to process. GetLastError(): " << GetLastError() << "\n";
         return false;
     }
 
     HANDLE hThread = CreateRemoteThread(hTarget, NULL, NULL, (LPTHREAD_START_ROUTINE)loadlib, dllPathAddr, NULL, NULL);
     if (hThread == NULL)
     {
-        std::cout << "Failed to create a thread in the target process. GetLastError(): " << GetLastError() << "\n";
+        cout << "Failed to create a thread in the target process. GetLastError(): " << GetLastError() << "\n";
         return false;
     }
 
@@ -64,7 +65,7 @@ bool InjectStandard(HANDLE hTarget, const char* dllpath)
 
     if (exit_code == 0)
     {
-        std::cout << "LoadLibrary failed.\n";
+        cout << "LoadLibrary failed.\n";
         return false;
     }
     return true;
@@ -73,17 +74,17 @@ bool InjectStandard(HANDLE hTarget, const char* dllpath)
 std::optional<std::string> read_whole_file(const fs::path& file)
 try
 {
-    std::stringstream buf;
-    std::ifstream ifs(file);
+    stringstream buf;
+    ifstream ifs(file);
     if (!ifs.is_open())
-        return std::nullopt;
-    ifs.exceptions(std::ios::failbit);
+        return nullopt;
+    ifs.exceptions(ios::failbit);
     buf << ifs.rdbuf();
     return buf.str();
 }
 catch (const std::ios::failure&)
 {
-    return std::nullopt;
+    return nullopt;
 }
 
 std::optional<fs::path> this_dir()
@@ -93,13 +94,13 @@ std::optional<fs::path> this_dir()
     if (!GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT, (LPCTSTR)&this_dir, &mod))
     {
         printf("GetModuleHandleEx failed (%i)\n", GetLastError());
-        return std::nullopt;
+        return nullopt;
     }
 
     if (!GetModuleFileName(mod, path, MAX_PATH))
     {
         printf("GetModuleFileName failed (%i)\n", GetLastError());
-        return std::nullopt;
+        return nullopt;
     }
 
     return fs::path(path).remove_filename();
@@ -118,26 +119,26 @@ int main()
         system("pause");
         return 0;
     }
-    std::string exe_path;
+    string exe_path;
     auto settings_path = current_dir.value() / "settings.txt";
     if (!fs::is_regular_file(settings_path))
     {
         /*printf("settings.txt not found\n");
         system("pause");*/
-        std::ofstream settings_file("settings.txt", std::ios_base::app);
+        ofstream settings_file("settings.txt", ios_base::app);
         if (settings_file.is_open()) {
-            settings_file << exe_path << std::endl;
+            settings_file << exe_path << endl;
             settings_file.close();
         }
         else {
             // Try to create the file if it doesn't exist
             std::ofstream create_file("settings.txt");
             if (create_file.is_open()) {
-                create_file << exe_path << std::endl;
+                create_file << exe_path << endl;
                 create_file.close();
             }
             else {
-                std::cout << "Error: Unable to create settings file." << std::endl;
+                cout << "Error: Unable to create settings file." << endl;
                 return 1;
             }
         }
@@ -153,11 +154,11 @@ int main()
     }
 
     //std::string exe_path;
-    std::getline(std::stringstream(settings.value()), exe_path);
+    getline(std::stringstream(settings.value()), exe_path);
     if (!fs::is_regular_file(exe_path))
     {
-        std::cout << "File path in settings.exe invalid" << std::endl;
-        std::cout << "Please select your Game Executable" << std::endl;
+        cout << "File path in settings.exe invalid" << endl;
+        cout << "Please select your Game Executable" << endl;
        /* printf("Target executable not found\n");
         system("pause");*/
         OPENFILENAMEA ofn{};
@@ -176,18 +177,18 @@ int main()
         if (GetOpenFileNameA(&ofn))
         {
             exe_path = ofn.lpstrFile;
-            std::ofstream settings_file("settings.txt", std::ios_base::out);
+            ofstream settings_file("settings.txt", std::ios_base::out);
             if (settings_file.is_open()) {
-                settings_file << exe_path << std::endl;
+                settings_file << exe_path << endl;
                 settings_file.close();
             }
             else {
-                std::cout << "Error: Unable to open settings file." << std::endl;
+                cout << "Error: Unable to open settings file." << endl;
                 return 1;
             }
         }
         else {
-            std::cout << "Error: Unable to open file dialog." << std::endl;
+            cout << "Error: Unable to open file dialog." << endl;
             return 1;
         }
 
